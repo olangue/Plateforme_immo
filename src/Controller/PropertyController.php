3ec2aea11fd430e82165884535ad46f7770c17b7
BOUNDA BONDA <olangue.bounda-bonda@labo-ve.fr>
2020-08-31 14:22:25 +0200

    home page and acheter page

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/biens", name="property.index")
+     */
+    public function index()
+    {
+        return $this->render('property/index.html.twig', [
+            'current_menu'=>'properties',
+            'controller_name' => 'PropertyController',
+        ]);
+    }
+}
